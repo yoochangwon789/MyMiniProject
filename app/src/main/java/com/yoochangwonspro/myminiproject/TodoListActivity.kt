b01e77ee3d4325ca33301ba15317d63be93e02e7
CN <ckddnjs554@naver.com>
2021-07-17 20:00:54 +0900

    Feat : 배경색 color 변수 생성 작업 및 TodoListPage 버튼 클릭시 TodoList 페이지 이동 Intent 작업

---
+++ app/src/main/java/com/yoochangwonspro/myminiproject/TodoListActivity.kt
@@ -0,0 +1,11 @@
+package com.yoochangwonspro.myminiproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TodoListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_todo_list)
+    }
+}
