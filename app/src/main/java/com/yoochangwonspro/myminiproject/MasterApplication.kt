bd7a4fca063db74f7b3634094c1f600a381be32c
CN <ckddnjs554@naver.com>
2021-07-19 00:01:18 +0900

    Feat : Manifest 권한 설정 및 application name 설정

---
+++ app/src/main/java/com/yoochangwonspro/myminiproject/MasterApplication.kt
@@ -0,0 +1,10 @@
+package com.yoochangwonspro.myminiproject
+
+import android.app.Application
+
+class MasterApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
