1c22de63fff802edb91fbc7c0e34c586a2b2b8cf
CN <ckddnjs554@naver.com>
2021-07-19 02:16:25 +0900

    Feat : OutStagramLoginActivity 디자인 구성

---
+++ app/src/main/java/com/yoochangwonspro/myminiproject/OutStagramLoginActivity.kt
@@ -0,0 +1,11 @@
+package com.yoochangwonspro.myminiproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OutStagramLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_out_stagram_login)
+    }
+}
