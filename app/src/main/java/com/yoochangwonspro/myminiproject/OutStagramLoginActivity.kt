4d196103b7ed34303f356efd8c723255863938e3
CN <ckddnjs554@naver.com>
2021-07-19 02:18:38 +0900

    Feat : OutStagramLoginActivity home 버튼 기능 추가

--- app/src/main/java/com/yoochangwonspro/myminiproject/OutStagramLoginActivity.kt
+++ app/src/main/java/com/yoochangwonspro/myminiproject/OutStagramLoginActivity.kt
@@ -1,11 +1,24 @@
 package com.yoochangwonspro.myminiproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yoochangwonspro.myminiproject.databinding.ActivityOutStagramLoginBinding
 
 class OutStagramLoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOutStagramLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityOutStagramLoginBinding.inflate(layoutInflater)
+        val view = binding.root
         setContentView(R.layout.activity_out_stagram_login)
+
+        binding.outStagramHomeView.setOnClickListener {
+            startActivity(
+                Intent(this, MainActivity::class.java)
+            )
+        }
     }
 }
