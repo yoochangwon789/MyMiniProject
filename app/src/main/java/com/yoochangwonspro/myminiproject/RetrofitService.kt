a5747e55a296dd7d334004bbf5060daecdcae8ed
CN <ckddnjs554@naver.com>
2021-07-19 00:39:14 +0900

    Feat : RetrofitService 회원 가입 POST API 추가

--- app/src/main/java/com/yoochangwonspro/myminiproject/RetrofitService.kt
+++ app/src/main/java/com/yoochangwonspro/myminiproject/RetrofitService.kt
@@ -1,6 +1,17 @@
 package com.yoochangwonspro.myminiproject
 
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
 interface RetrofitService {
 
-
+    @POST("user/signup/")
+    @FormUrlEncoded
+    fun register(
+        @Field("username") username: String,
+        @Field("password1") password1: String,
+        @Field("password2") password2: String
+    ): Call<User>
 }
