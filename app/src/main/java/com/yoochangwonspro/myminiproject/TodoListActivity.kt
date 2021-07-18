d07ea4b96ecd4da46203c9f42781ab2ad44dc4b3
CN <ckddnjs554@naver.com>
2021-07-18 19:42:42 +0900

    Feat : TodoList toggleButton Recyclerview 적용 작업

--- app/src/main/java/com/yoochangwonspro/myminiproject/TodoListActivity.kt
+++ app/src/main/java/com/yoochangwonspro/myminiproject/TodoListActivity.kt
@@ -1,6 +1,8 @@
 package com.yoochangwonspro.myminiproject
 
 import android.content.Intent
+import android.graphics.Paint
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -41,17 +43,18 @@ class TodoListActivity : AppCompatActivity() {
             model.addTodoList(TodoList(binding.todolistEditTextView.text.toString()))
             adapter.notifyDataSetChanged()
         }
-
     }
 }
 
 data class TodoList(val text: String, var isDone: Boolean = false)
 
 class TodoListAdapter(
-    private val dataSet: ArrayList<TodoList>
+    private val dataSet: ArrayList<TodoList>,
+    private val toggleButton: (todo: TodoList) -> Unit
 ) : RecyclerView.Adapter<TodoListAdapter.ViewHolder>() {
 
-    class ViewHolder(val itemViewBinding: TodolistItemViewBinding) : RecyclerView.ViewHolder(itemViewBinding.root)
+    class ViewHolder(val itemViewBinding: TodolistItemViewBinding) :
+        RecyclerView.ViewHolder(itemViewBinding.root)
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(viewGroup.context)
@@ -61,13 +64,30 @@ class TodoListAdapter(
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        viewHolder.itemViewBinding.todolistItemTextView.text = dataSet[position].text
+        val todo = dataSet[position]
+        viewHolder.itemViewBinding.todolistItemTextView.text = todo.text
+
+        if (todo.isDone) {
+            viewHolder.itemViewBinding.todolistItemTextView.apply {
+                paintFlags = paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+                setTypeface(null, Typeface.ITALIC)
+            }
+        } else {
+            viewHolder.itemViewBinding.todolistItemTextView.apply {
+                paintFlags = 0
+                setTypeface(null, Typeface.NORMAL)
+            }
+        }
+
+        viewHolder.itemViewBinding.root.setOnClickListener {
+            toggleButton(todo)
+        }
     }
 
     override fun getItemCount() = dataSet.size
 }
 
-class TodoListViewModel: ViewModel() {
+class TodoListViewModel : ViewModel() {
 
     val todoListData = ArrayList<TodoList>()
 
