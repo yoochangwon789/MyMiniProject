ae6269d9c49cc11fa3b5a3ec1febf3ff41b72546
CN <ckddnjs554@naver.com>
2021-07-19 00:33:21 +0900

    Feat : MasterApplication createRetrofit 함수 생성 과 header Interceptor 기능 구현

--- app/src/main/java/com/yoochangwonspro/myminiproject/MasterApplication.kt
+++ app/src/main/java/com/yoochangwonspro/myminiproject/MasterApplication.kt
@@ -2,11 +2,29 @@ package com.yoochangwonspro.myminiproject
 
 import android.app.Application
 import android.content.Context
+import okhttp3.Interceptor
 
 class MasterApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+    }
+
+    fun createRetrofit() {
+        val header = Interceptor {
+            val original = it.request()
+
+            if (checkIsLogin()) {
+                getUserToken()?.let { token ->
+                    val request = original.newBuilder()
+                        .header("Authorization", "token " + token)
+                        .build()
+                    it.proceed(request)
+                }
+            } else{
+                it.proceed(original)
+            }
+        }
     }
 
     fun checkIsLogin(): Boolean {
