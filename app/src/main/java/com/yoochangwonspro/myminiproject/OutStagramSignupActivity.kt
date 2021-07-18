bea962a0fc43b97f01f9816ee378e15aa6722f34
CN <ckddnjs554@naver.com>
2021-07-19 00:42:12 +0900

    Feat : OutStagramSignupActivity view binding 셋팅

--- app/src/main/java/com/yoochangwonspro/myminiproject/OutStagramSignupActivity.kt
+++ app/src/main/java/com/yoochangwonspro/myminiproject/OutStagramSignupActivity.kt
@@ -2,10 +2,16 @@ package com.yoochangwonspro.myminiproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yoochangwonspro.myminiproject.databinding.ActivityOutStagramSignupBinding
 
 class OutStagramSignupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOutStagramSignupBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_out_stagram_signup)
+        binding = ActivityOutStagramSignupBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
