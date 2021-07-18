ea0caedf53eb7f2ee1bdf31bc275d6f3de8e6d6b
CN <ckddnjs554@naver.com>
2021-07-19 01:14:38 +0900

    Feat : OutStagramSignupActivity signUpRetrofit 함수 생성

--- app/src/main/java/com/yoochangwonspro/myminiproject/MasterApplication.kt
+++ app/src/main/java/com/yoochangwonspro/myminiproject/MasterApplication.kt
@@ -2,6 +2,7 @@ package com.yoochangwonspro.myminiproject
 
 import android.app.Application
 import android.content.Context
+import com.facebook.stetho.Stetho
 import com.facebook.stetho.okhttp3.StethoInterceptor
 import com.google.gson.Gson
 import okhttp3.Interceptor
@@ -15,6 +16,9 @@ class MasterApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        Stetho.initializeWithDefaults(this)
+        createRetrofit()
     }
 
     fun createRetrofit() {
--- app/src/main/java/com/yoochangwonspro/myminiproject/OutStagramSignupActivity.kt
+++ app/src/main/java/com/yoochangwonspro/myminiproject/OutStagramSignupActivity.kt
@@ -3,7 +3,12 @@ package com.yoochangwonspro.myminiproject
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.yoochangwonspro.myminiproject.databinding.ActivityOutStagramSignupBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class OutStagramSignupActivity : AppCompatActivity() {
 
@@ -15,6 +20,36 @@ class OutStagramSignupActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+    }
+
+
+
+    fun signUpRetrofit() {
+        (application as MasterApplication).service.register(
+            idText,
+            passwordOne,
+            passwordTwo
+        ).enqueue(object : Callback<User> {
+            override fun onResponse(call: Call<User>, response: Response<User>) {
+                if (response.isSuccessful) {
+                    val user = response.body()
+                    saveUserToken(user!!.token!!)
+                    Toast.makeText(
+                        this@OutStagramSignupActivity,
+                        "가입이 되었습니다.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+
+            override fun onFailure(call: Call<User>, t: Throwable) {
+                Toast.makeText(
+                    this@OutStagramSignupActivity,
+                    "가입에 실패했습니다.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        })
     }
 
     fun saveUserToken(token: String) {
