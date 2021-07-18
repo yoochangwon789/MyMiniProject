8f98cf8b713f31ef39178257437f82e54633cc59
CN <ckddnjs554@naver.com>
2021-07-19 00:20:55 +0900

    Feat : MasterApplication getUserToken 함수 구현

--- app/src/main/java/com/yoochangwonspro/myminiproject/MasterApplication.kt
+++ app/src/main/java/com/yoochangwonspro/myminiproject/MasterApplication.kt
@@ -13,6 +13,14 @@ class MasterApplication : Application() {
         val sp = getSharedPreferences("login_sp", Context.MODE_PRIVATE)
         val token = sp.getString("login_sp", "null")
 
-        return (token != "null")
+        return token != "null"
+    }
+
+    fun getUserToken(): String? {
+        val sp = getSharedPreferences("login_sp", Context.MODE_PRIVATE)
+        val token = sp.getString("login_sp", "null")
+
+        return if (token == "null") null
+        else token
     }
 }
