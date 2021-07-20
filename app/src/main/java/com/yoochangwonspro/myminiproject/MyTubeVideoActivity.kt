fdafde63b26065a15bb189c08234a66f91e31172
CN <ckddnjs554@naver.com>
2021-07-20 20:34:04 +0900

    Feat : MyTubeVideoActivity 비디오 재생 기능 추가

--- app/src/main/java/com/yoochangwonspro/myminiproject/MyTubeVideoActivity.kt
+++ app/src/main/java/com/yoochangwonspro/myminiproject/MyTubeVideoActivity.kt
@@ -2,10 +2,19 @@ package com.yoochangwonspro.myminiproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.MediaController
+import kotlinx.android.synthetic.main.activity_my_tube_video.*
 
 class MyTubeVideoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_tube_video)
+
+        val url = intent.getStringExtra("video_url")
+        val mediaController = MediaController(this)
+        my_tube_video_view.setVideoPath(url)
+        my_tube_video_view.requestFocus()
+        my_tube_video_view.start()
+        mediaController.show()
     }
 }
