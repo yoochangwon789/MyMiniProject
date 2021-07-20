123e8e29e191a54387d2dd3e8126073f3a300f01
CN <ckddnjs554@naver.com>
2021-07-20 20:50:42 +0900

    Feat : Song 객체 추가

---
+++ app/src/main/java/com/yoochangwonspro/myminiproject/Song.kt
@@ -0,0 +1,9 @@
+package com.yoochangwonspro.myminiproject
+
+import java.io.Serializable
+
+class Song(
+    var title: String? = null,
+    var thumbnail: String? = null,
+    var song: String? = null
+) : Serializable
