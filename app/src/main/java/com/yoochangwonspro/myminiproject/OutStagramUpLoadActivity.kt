b253ccb766e8a80839f7eef30d6a4ae124c486bc
CN <ckddnjs554@naver.com>
2021-07-20 17:09:41 +0900

    Feat : OutStagramUpLoadActivity view binding 작업

--- app/src/main/java/com/yoochangwonspro/myminiproject/OutStagramUpLoadActivity.kt
+++ app/src/main/java/com/yoochangwonspro/myminiproject/OutStagramUpLoadActivity.kt
@@ -2,10 +2,16 @@ package com.yoochangwonspro.myminiproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yoochangwonspro.myminiproject.databinding.ActivityOutStagramUpLoadBinding
 
 class OutStagramUpLoadActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOutStagramUpLoadBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_out_stagram_up_load)
+        binding = ActivityOutStagramUpLoadBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
