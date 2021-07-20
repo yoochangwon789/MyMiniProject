f0fb4b1aea94c46a86e6c1a798aa04eb527bbe5c
CN <ckddnjs554@naver.com>
2021-07-20 20:41:50 +0900

    Feat : SongList 디자인 구성

---
+++ app/src/main/java/com/yoochangwonspro/myminiproject/SongListActivity.kt
@@ -0,0 +1,11 @@
+package com.yoochangwonspro.myminiproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SongListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_song_list)
+    }
+}
