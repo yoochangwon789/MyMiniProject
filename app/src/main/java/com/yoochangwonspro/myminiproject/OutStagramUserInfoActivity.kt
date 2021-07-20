7148b115dfe9117806575b3689d3bc6691baf2d4
CN <ckddnjs554@naver.com>
2021-07-20 19:34:00 +0900

    Feat : OutStagramUserInfoActivity 하단 카테고리 버튼 기능 추가

--- app/src/main/java/com/yoochangwonspro/myminiproject/OutStagramUserInfoActivity.kt
+++ app/src/main/java/com/yoochangwonspro/myminiproject/OutStagramUserInfoActivity.kt
@@ -1,11 +1,42 @@
 package com.yoochangwonspro.myminiproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yoochangwonspro.myminiproject.databinding.ActivityOutStagramUserInfoBinding
 
 class OutStagramUserInfoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOutStagramUserInfoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_out_stagram_user_info)
+        binding = ActivityOutStagramUserInfoBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.userInfoHomeBtn.setOnClickListener {
+            startActivity(
+                Intent(this, MainActivity::class.java)
+            )
+        }
+
+        binding.userInfoAllPostView.setOnClickListener {
+            startActivity(
+                Intent(this, OutStagramPostListActivity::class.java)
+            )
+        }
+
+        binding.userInfoUpLoadView.setOnClickListener {
+            startActivity(
+                Intent(this, OutStagramUpLoadActivity::class.java)
+            )
+        }
+
+        binding.userInfoUserPostView.setOnClickListener {
+            startActivity(
+                Intent(this, OutStagramMyPostActivity::class.java)
+            )
+        }
     }
 }
