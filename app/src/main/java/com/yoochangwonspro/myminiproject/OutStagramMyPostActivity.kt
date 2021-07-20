0b5d2fb2eb4be6659c839c105114bbc1f518c82a
CN <ckddnjs554@naver.com>
2021-07-20 19:12:11 +0900

    Feat : OutStagramMyPostActivity MyPostAdapter 구현

--- app/src/main/java/com/yoochangwonspro/myminiproject/OutStagramMyPostActivity.kt
+++ app/src/main/java/com/yoochangwonspro/myminiproject/OutStagramMyPostActivity.kt
@@ -1,9 +1,15 @@
 package com.yoochangwonspro.myminiproject
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.yoochangwonspro.myminiproject.databinding.ActivityOutStagramMyPostBinding
+import com.yoochangwonspro.myminiproject.databinding.PostListItemViewBinding
 
 class OutStagramMyPostActivity : AppCompatActivity() {
 
@@ -38,5 +44,33 @@ class OutStagramMyPostActivity : AppCompatActivity() {
                 Intent(this, OutStagramUserInfoActivity::class.java)
             )
         }
+
+
     }
+}
+
+class MyPostListAdapter(
+    private val dataSet: ArrayList<Post>,
+    private val activity: Activity
+) : RecyclerView.Adapter<MyPostListAdapter.ViewHolder>() {
+
+    class ViewHolder(val itemViewBinding: PostListItemViewBinding) :
+        RecyclerView.ViewHolder(itemViewBinding.root) {}
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.post_list_item_view, viewGroup, false)
+
+        return ViewHolder(PostListItemViewBinding.bind(view))
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.itemViewBinding.postListOwnerItem.text = dataSet[position].owner
+        viewHolder.itemViewBinding.postListContentItem.text = dataSet[position].content
+        Glide.with(activity)
+            .load(dataSet[position].image)
+            .into(viewHolder.itemViewBinding.postListImgItem)
+    }
+
+    override fun getItemCount() = dataSet.size
 }
